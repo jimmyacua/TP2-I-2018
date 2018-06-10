baeeed82dfd43d3e4a999aedb72926f29bc579fa
jimmyacua <jimmyacua@hotmail.com>
2018-06-10 13:36:30 -0600

    Primer commit

---
+++ Relacion1a1.h
@@ -0,0 +1,8 @@
+//
+// Created by jimmy on 10/06/18.
+//
+
+#ifndef TP2_I_2018_RELACION1A1_H
+#define TP2_I_2018_RELACION1A1_H
+
+#endif //TP2_I_2018_RELACION1A1_H
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by jimmy on 10/06/18.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hola putos" << endl;
+
+    return 0;
+}
